437877554c5cbdfe9149446e5a1e9ccc469a2643
Karol <45574534+chojnacky@users.noreply.github.com>
2024-01-10 21:27:14 +0100

    Add files via upload

--- OneRent Oddziały.user.js
+++ OneRent Oddziały.user.js
@@ -11,28 +11,29 @@
 
     // Definicja tablicy z oddziałami
     var oddzialy = [
-        { name: 'Gorzów Wielkopolski', address: 'ul. Mieszka I 65' },
-        { name: 'Szczecin', address: 'ul. Torfowa 3a' },
-        { name: 'Zielona Góra', address: 'ul. Kręta 2' },
-        { name: 'Koszalin', address: 'ul. Andersa 16a' },
-        { name: 'Poznań', address: 'ul. Murawy 12' },
-        { name: 'Gdańsk', address: 'ul. Grunwaldzka 339' },
-        { name: 'Słupsk', address: 'ul. Leszczyńskiego 10A' },
-        { name: 'Warszawa', address: 'ul. Ciszewskiego 7' },
-        { name: 'Leszno', address: 'ul. 3 maja 12' },
-        { name: 'Lubin', address: 'ul. Curie-Skłodowskiej 172' },
-        { name: 'Jelenia Góra', address: 'ul. Zgorzelecka 9' },
-        { name: 'Wrocław', address: 'ul. Olsztyńska 2' },
-        { name: 'Gryfice', address: 'ul. Piłsudskiego 34A' },
-        { name: 'Piła', address: 'ul. Grunwaldzka 2' },
-        { name: 'Giżycko', address: 'ul. Kolejowa 18' },
-        { name: 'Łódź', address: 'ul. Częstochowska 43' },
-        { name: 'Chojnice', address: 'ul. Bydgoska 16' },
-        { name: 'Lublin', address: 'ul. Abramowice Prywatne 42B' },
-        { name: 'Chorzów', address: 'ul. Katowicka 160' },
-        { name: 'Kraków', address: 'ul. Dobrego Pasterza 43' },
-        { name: 'Toruń', address: 'ul. Polna 128/132' },
-        { name: 'Katowice', address: 'ul. Jankego 11' }
+        { region: 'Północ', name: 'Gorzów Wielkopolski', address: 'ul. Mieszka I 65' },
+        { region: 'Północ', name: 'Szczecin', address: 'al. Wojska Polskiego 70' },
+        { region: 'Północ', name: 'Koszalin', address: 'ul. Andersa 16a' },
+        { region: 'Północ', name: 'Gdańsk', address: 'ul. Grunwaldzka 339' },
+        { region: 'Północ', name: 'Słupsk', address: 'ul. Leszczyńskiego 10A' },
+        { region: 'Północ', name: 'Gryfice', address: 'ul. Piłsudskiego 34A' },
+        { region: 'Północ', name: 'Piła', address: 'ul. Grunwaldzka 2' },
+        { region: 'Północ', name: 'Chojnice', address: 'ul. Bydgoska 16' },
+        { region: 'Północ', name: 'Giżycko', address: 'ul. Kolejowa 18' },
+        { region: 'Zachód', name: 'Zielona Góra', address: 'ul. Kręta 2' },
+        { region: 'Zachód', name: 'Poznań', address: 'ul. Murawy 12' },
+        { region: 'Zachód', name: 'Leszno', address: 'ul. 3 maja 12' },
+        { region: 'Zachód', name: 'Lubin', address: 'ul. Curie-Skłodowskiej 172' },
+        { region: 'Zachód', name: 'Jelenia Góra', address: 'ul. Zgorzelecka 9' },
+        { region: 'Zachód', name: 'Toruń', address: 'ul. Polna 128/132' },
+        { region: 'Zachód', name: 'Bydgoszcz', address: 'ul. Z. Augusta 7' },
+        { region: 'Wschód', name: 'Warszawa', address: 'ul. Ciszewskiego 7' },
+        { region: 'Wschód', name: 'Łódź', address: 'ul. Częstochowska 43' },
+        { region: 'Wschód', name: 'Lublin', address: 'ul. Abramowice Prywatne 42B' },
+        { region: 'Południe', name: 'Wrocław', address: 'ul. Olsztyńska 2' },
+        { region: 'Południe', name: 'Chorzów', address: 'ul. Katowicka 160' },
+        { region: 'Południe', name: 'Kraków', address: 'ul. Dobrego Pasterza 43' },
+        { region: 'Południe', name: 'Katowice', address: 'ul. Jankego 11' }
         // Dodaj pozostałe oddziały według tego samego wzoru
     ];
 
@@ -42,7 +43,7 @@
         startElements.forEach(function(element) {
             oddzialy.forEach(function(oddzial) {
                 if (element.textContent.trim() === oddzial.name) {
-                    element.textContent = oddzial.name + ', ' + oddzial.address;
+                    element.textContent = '[' + oddzial.region + '] ' + oddzial.name + ', ' + oddzial.address;
                 }
             });
         });
@@ -52,7 +53,7 @@
         endElements.forEach(function(element) {
             oddzialy.forEach(function(oddzial) {
                 if (element.textContent.trim() === oddzial.name) {
-                    element.textContent = oddzial.name + ', ' + oddzial.address;
+                    element.textContent = '[' + oddzial.region + '] ' + oddzial.name + ', ' + oddzial.address;
                 }
             });
         });
